8da8b30c220eaa91bf84cb6406e0a6db7cec1216
Ruter Lyu <ruter.lv@gmail.com>
2023-03-31 16:07:04 +0800

    add hidden class when unlocked

--- src/routes/share/[slug]/+page.svelte
+++ src/routes/share/[slug]/+page.svelte
@@ -50,6 +50,7 @@
       <button
         type="button"
         class="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-700"
+        class:hidden={unlocked}
         on:click={() => (isModalOpen = true)}>
         <KeyIcon class="mr-1.5 h-4 w-4 text-white" />
         Unlock
