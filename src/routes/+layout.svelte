b5905ab7a990c4522e70888954a8a572ce3bd726
Ruter Lyu <ruter.lv@gmail.com>
2023-04-02 14:01:56 +0800

    update icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,10 +9,7 @@
       <div class="flex h-16 items-center justify-between">
         <div class="flex items-center">
           <div class="flex-shrink-0">
-            <img
-              class="h-8 w-8"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-              alt="SpaceBin" />
+            <a href="/"><img class="h-10 w-10" src="/icon.svg" alt="SpaceBin" /></a>
           </div>
         </div>
         <div>
